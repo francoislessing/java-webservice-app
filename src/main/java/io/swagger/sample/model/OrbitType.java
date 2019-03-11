0a5df53bd65cbe60cf091231aebdd1a504ca9ea5
Francois Lessing <francois.lessing@gmail.com>
2019-03-11 13:13:02 +0200

    Added orbit type to be used later on as part of Satellite data

---
+++ src/main/java/io/swagger/sample/model/OrbitType.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.swagger.sample.model;
+
+
+/**
+ *
+ * @author francois
+ */
+
+public class OrbitType  {
+  public long  a0;
+  public long  a1;
+  public float deltaInclination;
+  public float eccentricity;
+  public float gpsSeconds;
+  public float gpsWeek;
+  public int   healthCode;
+  public boolean isHealthy;
+  public float m0;
+  public float omega;
+  public float omega0;
+  public float omegaDot;
+  public float rootOfSemiMajorAxis;
+  public long  satId;
+    
+}
